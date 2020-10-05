ba9df7ca445071c44b2a6dcc8c58e1322874dc73
Emma <slicey2001@gmail.com>
2020-10-05 13:20:00 +0000

    statix

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_miatoll.mk
+    $(LOCAL_DIR)/statix_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_miatoll-user \
-    lineage_miatoll-userdebug \
-    lineage_miatoll-eng
+    statix_miatoll-user \
+    statix_miatoll-userdebug \
+    statix_miatoll-eng
